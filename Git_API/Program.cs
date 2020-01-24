90590b9ba927bf57658fdfd632c884d4d2c8eb75
skathanas <32923297+skathanas@users.noreply.github.com>
2020-01-24 12:22:11 +0200

    Add files via upload

---
+++ Git_API/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Threading.Tasks;
+using Nancy.Json;
+using Newtonsoft.Json;
+
+
+namespace Git_API
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, insert github username: ");
+            var username = Console.ReadLine();
+            string client_ID = "8d3f4863d00401a49bd2";
+            string client_secret = "a8a8786dc0aa8538f951aa1e18f61d48cdb436c2";
+            var repos_count = 10;
+            string repos_sort = "created: asc";
+            
+
+            string apiUrl = "https://api.github.com/users/";
+
+            string gitHubUrl = apiUrl + username + "?client_id=" + client_ID + "&client_secret=" + client_secret;
+            
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(gitHubUrl);
+            request.Method = "GET";
+            request.UserAgent = "foo";
+
+
+
+
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+            using (var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                GitHub gInfo = JsonConvert.DeserializeObject<GitHub>(response);
+                Console.WriteLine($"Kasutajanimi: {gInfo.login}");
+                Console.WriteLine($"Kasutaja ID: {gInfo.id}");
+                Console.WriteLine($"Kasutaja URL: {gInfo.html_url}");
+                Console.WriteLine($"Kasutaja e-mail: {gInfo.email}");
+                Console.WriteLine($"Kasutaja repod: {gInfo.public_repos}");
+                Console.WriteLine($"Kasutaja jälgijad: {gInfo.follorwers}");
+                Console.WriteLine($"Kasutaja jälgib: {gInfo.following}");
+                Console.WriteLine($"Otselink APIle: {gitHubUrl}");
+            }
+        }
+       
+
+    }
+}
+
---
+++ Git_API/Properties/GitHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Threading.Tasks;
+using Nancy.Json;
+
+namespace Git_API
+
+{
+    class GitHub
+    {
+        public string login { get; set; }
+        public string id { get; set; }
+        public string html_url { get; set; }
+        public string public_repos { get; set; }
+        public string follorwers { get; set; }
+        public string following { get; set; }
+        public string email { get; set; }
+    }
+}
